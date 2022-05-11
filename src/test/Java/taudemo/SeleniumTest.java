ab724b9a5eaf7dee7d963757206768513c12a015
Jorge Criado Ros <workbronce@hotmail.com>
2022-05-11 14:15:09 +0200

    Auto Import Class Reformat

--- src/test/Java/taudemo/SeleniumTest.java
+++ src/test/Java/taudemo/SeleniumTest.java
@@ -5,23 +5,28 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SeleniumTest {
-    private BrowserGetter browserGetter =  new BrowserGetter();
+    private BrowserGetter browserGetter = new BrowserGetter();
     private WebDriver driver;
 
     @BeforeAll
-    public void beforeAll(){
+    public void beforeAll() {
+
         driver = browserGetter.getChromeDriver();
     }
 
     @AfterAll
-    public void afterAll(){
+    public void afterAll() {
+
         driver.quit();
     }
 
     @Test
-    public void openThePageAndCheckTheTitle(){
+    public void openThePageAndCheckTheTitle() {
         String expectedTitle = "Example title";
-
+        driver.get("https://www.example.com");
+        assertEquals(expectedTitle, driver.getTitle());
     }
 }
