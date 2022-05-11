16956999ba04555dcd2a172f3881812160c42788
Jorge Criado Ros <workbronce@hotmail.com>
2022-05-11 13:41:39 +0200

    Creating the Test Methods

--- Java/taudemo/SeleniumTest.java
+++ Java/taudemo/SeleniumTest.java
@@ -1,4 +1,22 @@
 package Java.taudemo;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class SeleniumTest {
+    @BeforeAll
+    public void beforeAll(){
+
+    }
+
+    @AfterAll
+    public void afterAll(){
+
+    }
+
+    @Test
+    public void openThePageAndCheckTheTitle(){
+
+    }
 }
